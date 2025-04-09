9b55da960164e29b722cce73491cc9352d0cdb26
Aravindhan V <aravindhanvgc@gmail.com>
2025-04-09 13:31:13 +0530

    mongo url updated to atlas

--- server/database/db.js
+++ server/database/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDb = async ()=>{
     try{
-        await mongoose.connect("mongodb://127.0.0.1:27017/pc-bot");
+        await mongoose.connect("mongodb+srv://aravind:admin8204@cluster0.nl387uc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
         console.log("Db connection established");
     }
     catch(e){
