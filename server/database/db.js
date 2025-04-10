a86d4431b335b08a476e4d44ca61287368af1656
Aravindhan V <aravindhanvgc@gmail.com>
2025-04-10 11:47:26 +0530

    adding cors

--- server/database/db.js
+++ server/database/db.js
@@ -4,7 +4,7 @@ const connectDb = async ()=>{
     try{
         // await mongoose.connect("mongodb://127.0.0.1:27017/pc-bot")
         await mongoose.connect("mongodb+srv://aravind:admin8204@cluster0.nl387uc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
-        console.log("Db connection established");
+        //console.log("Db connection established");
     }
     catch(e){
         console.log(e.message);
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const { connectDb } = require("./database/db.js");
 const route = require('./routes/route');
+const cors = require("cors");
 
 
 const app = express();
@@ -9,6 +10,8 @@ connectDb();
 
 //middleware
 app.use(express.json());
+app.use(cors());
+
 
 //initial testing
 app.use("/test", (req,res)=>{
